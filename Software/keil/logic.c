d8ff3db813408c007cd283ea34f08deb41a1cc1e
GargantuaInfinity <Gargantua.X@outlook.com>
2023-03-06 11:08:51 +0800

    逻辑函数定义

--- Software/keil/logic.c
+++ Software/keil/logic.c
@@ -1,1 +1,88 @@
 #include "logic.h"
+
+uint16_t logic_random_num(uint16_t max_num, uint16_t min_num)
+{
+    uint16_t a = 1103515245;  // LCG算法参数a
+    uint16_t c = 12345;       // LCG算法参数c
+    uint16_t m = 2147483648;  // LCG算法参数m
+    uint16_t x = 1;           // 初始随机数值为1
+    uint16_t rand_num=0;
+    uint16_t range=max_num-min_num;
+    for (int16_t i = 0; i < 10; i++) {
+        x = (a * x + c) % m;  // 生成新的随机数
+        rand_num = x % range + 1;  // 将生成的随机数映射到最大值和最小值之间
+        rand_num = rand_num + min_num;
+    }
+    return rand_num;
+}
+/*利用LCG算法生成伪随机数，返回随机数值，数值在指定最大值和最小值之间*/
+
+void logic_rise_up(uint16_t speed, struct Logic_player_bird_position *bird_position)
+{
+    //uint16_t pos=bird_position->position;
+    for(uint16_t i=0; i<=3; i++)
+    {
+        if(bird_position->edge_y[i]+speed > LCD_Y)
+        {
+            bird_position->edge_y[i]=bird_position->edge_y[i];
+        }
+        else 
+        {
+            bird_position->edge_y[i]=bird_position->edge_y[i]+speed;
+        }    
+    }
+}
+/*以speed像素每秒速度上升*/
+
+void logic_descend_down(uint16_t speed, struct Logic_player_bird_position *bird_position)
+{
+    for(uint16_t i=0; i<=3; i++)
+    {
+        if(bird_position->edge_y[i]>speed)
+        {
+            bird_position->edge_y[i]=bird_position->edge_y[i]-speed;
+        }
+        else
+        {
+            bird_position->edge_y[i]=bird_position->edge_y[i];
+        }
+    }
+}
+/*以speed像素每秒速度下降*/
+
+uint16_t logic_collide(struct Logic_player_bird_position *bird_position, struct Logic_ocstacle_position *oc_pos,struct Logic_game_event *logic)
+{
+    struct Logic_player_bird_position bird = *bird_position;
+    struct Logic_ocstacle_position ocs = *oc_pos;
+    for(uint16_t i=0; i<=1; i++)
+    {
+        if(bird.edge_x[1]<ocs.oc_edge_position || bird.edge_x[0]>(ocs.oc_edge_position+ocs.ocstacle_width))
+        {
+            return 0;
+        }
+        else if(ocs.ocstacle_type==0)//下方的障碍物
+        {
+            if(bird.edge_y[3]>ocs.ocstacle_height)
+            {
+                return 0;
+            }
+            else if(bird.edge_y[3]<=ocs.ocstacle_height)
+            {
+                return 1;
+            }
+        }
+        else if(ocs.ocstacle_type==1)//上方的障碍物
+        {
+            if(bird.edge_y[0]<ocs.ocstacle_height)
+            {
+                return 0;
+            }
+            else if(bird.edge_y[0]>=ocs.ocstacle_height)
+            {
+                return 1;
+            }
+        }
+    }
+}
+/*判断是否碰撞，碰撞返回1，未碰撞返回0*/
+
--- Software/keil/logic.h
+++ Software/keil/logic.h
@@ -1,23 +1,28 @@
 #include <stdint.h>
 #include <stdio.h>
 #include "code_def.h"
+#define LCD_Y 480  //屏幕纵向像素高度
+#define LCD_X 800  //屏幕横向像素宽度
 
 /*本文件定义游戏运行逻辑*/
 
 struct Logic_game_event{
-    uint16_t Collide_return;//判断是否碰撞返回值
-    uint16_t Pause;//暂停，为1时暂停，为0时游戏继续进行
+    uint16_t Collide_return;//判断是否碰撞返回值,为1时碰撞，为0时未碰撞
+    uint16_t Pause;//暂停，为0时暂停，为1时游戏继续进行
+    uint16_t End;//游戏结束，为0时结束，为1时游戏继续进行
     //uint16_t  Score;//得分
 };//游戏相关事件，单局游戏中结构体只建立一次
 
 struct Logic_player_bird_position{
+    //uint16_t position;
     uint16_t edge_x[4];
     uint16_t edge_y[4];
 };//玩家所控制的鸟的碰撞体积,从左上角开始顺时针为0，1，2，3
 
 struct Logic_ocstacle_position{
     uint16_t ocstacle_id;
-    uint16_t ocstacle_height;
+    uint16_t ocstacle_type;  //0为下方障碍，1为上方障碍
+    uint16_t ocstacle_height;  //绝对高度，即障碍物边界在屏幕上的纵坐标
     uint16_t ocstacle_width;
     uint16_t oc_edge_position;//左侧边界横坐标
     struct Logic_ocstacle_position *next;
@@ -26,13 +31,13 @@ struct Logic_ocstacle_position{
 uint16_t logic_random_num(uint16_t max_num, uint16_t min_num);
 /*利用LCG算法生成伪随机数，返回随机数值*/
 
-void logic_rise_up(uint16_t speed);
+void logic_rise_up(uint16_t speed, struct Logic_player_bird_position *bird_position);
 /*以speed像素每秒速度上升*/
 
-void logic_descend_down(uint16_t speed);
+void logic_descend_down(uint16_t speed, struct Logic_player_bird_position *bird_position);
 /*以speed像素每秒速度下降*/
 
-uint16_t logic_collide(struct Logic_player_bird_position *player_position, uint16_t position_obstacle);
+uint16_t logic_collide(struct Logic_player_bird_position *bird_position, struct Logic_ocstacle_position *oc_pos, struct Logic_game_event *logic);
 /*判断是否碰撞，碰撞返回1，未碰撞返回0*/
 
 uint16_t logic_fly_right_speed(uint16_t position);
